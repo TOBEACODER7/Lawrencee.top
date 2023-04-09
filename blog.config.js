c03a3a8f306c11ff9b60f3b4bde397772630eeac
lawrencee <TOBE_ACODER@163.com>
2023-04-09 22:03:56 +0800

    Add: personal info

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'Lawrencee',
+  title: 'Lawrencee PageHome',
   author: 'Lawrence Yang',
   email: 'tobe_acoder@163.com',
   link: 'lawrencee.top',
-  description: 'This gonna be an awesome website.',
+  description: 'I`ll try my best to show all things I concerned.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
